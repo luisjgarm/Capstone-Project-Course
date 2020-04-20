d0a0ace1f4e25e4277d31ba0f69acbb2991ad0d2
ilahlou <bimito@yahoo.com>
2020-04-20 19:58:40 -0400

    Adds a python scipt to initiate the Capstone Project

---
+++ CapstoneProject_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **This notebook will be mainly used for the capstone project**
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print ("Hello Capstone Project Course!")
+
